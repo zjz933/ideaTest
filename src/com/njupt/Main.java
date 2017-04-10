8e9d40ac1de4fe761a4e58b355f81b8968623430
zjz933 <85262342@qq.com>
2017-04-10 11:20:30 +0800

    the first commit

---
+++ src/com/njupt/Main.java
@@ -0,0 +1,9 @@
+package com.njupt;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("This is a demo of idea and github");
+    }
+}
